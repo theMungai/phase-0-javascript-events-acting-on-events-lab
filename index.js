19c9e3f21f62a1b9ac09444b546eaed90f01d7b2
theMungai <mungaithesimon@gmail.com>
2025-03-01 16:56:15 +0300

    EventListeners in JS

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.querySelector("#dodger");
+
+            
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left -1}px`
+    }
+
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft()
+    }
+})
+
+
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10);
+    
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
